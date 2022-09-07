fd8f91b0a592b995a3f83dd732b4068b3249991c
zhanlutuzi <zhanlutuzi@gmail.com>
2022-09-07 17:17:02 +0800

    完成EX5

--- EX5/Form1.Designer.cs
+++ EX5/Form1.Designer.cs
@@ -29,13 +29,87 @@ namespace EX5
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "并行统计";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(427, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "线程二统计结果";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "线程一统计结果";
+            // 
+            // listView2
+            // 
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(430, 92);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(371, 497);
+            this.listView2.TabIndex = 6;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(31, 92);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(370, 497);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(813, 601);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "EX5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- EX5/Form1.cs
+++ EX5/Form1.cs
@@ -6,6 +6,11 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MapGIS.GeoDataBase;
+using System.Threading;
+using MapGIS.GeoObjects.Att;
+using MapGIS.GeoObjects;
+using MapGIS.GeoObjects.Geometry;
 
 namespace EX5
 {
@@ -14,6 +19,114 @@ namespace EX5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //定义数据源、数据库变量
+            Server Svr = null;
+            DataBase GDB = null;
+            SFeatureCls SFCls = null;
+
+            Svr = new Server();
+            //连接数据源
+            Svr.Connect("MapGisLocal", "", "");
+
+            GDB = new DataBase();
+            GDB = Svr.OpenGDB("空间信息高性能计算实验");
+            SFCls = new SFeatureCls(GDB);
+            //打开图层
+            SFCls.Open("道路", 1);
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+            var threadOne = new Thread(() => statistics(SFCls, listView1, 1));
+            threadOne.Name = "ThreadOne";
+            var threadTwo = new Thread(() => statistics(SFCls, listView2, 2));
+            threadTwo.Name = "ThreadTwo";
+
+            threadOne.Start();
+            threadTwo.Start();
+        }
+
+        private void statistics(SFeatureCls resource_sfcls, ListView listView, int L_or_R)
+        {
+            RecordSet _RecordSet = null;
+            IGeometry _Geometry = null;
+            QueryDef _QueryDef = new QueryDef();
+            Rect rect = new Rect();
+            SpaQueryMode mode = new SpaQueryMode();
+            GeoVarLine _GeoVarLine = new GeoVarLine();
+            GeoLines _GeoLines = new GeoLines();
+            long oid = 0;
+            double length = 0;
+
+            if (L_or_R == 1)
+            {
+                //左边的矩形框
+                rect.XMax = resource_sfcls.Range.XMin + (resource_sfcls.Range.XMax - resource_sfcls.Range.XMin) / 2;
+                rect.YMax = resource_sfcls.Range.YMax;
+                rect.YMin = resource_sfcls.Range.YMin;
+                rect.XMin = resource_sfcls.Range.XMin;
+                mode = SpaQueryMode.Intersect;
+
+            }
+            else
+            {
+                rect.XMax = resource_sfcls.Range.XMax;
+                rect.YMax = resource_sfcls.Range.YMax;
+                rect.YMin = resource_sfcls.Range.YMin;
+                rect.XMin = resource_sfcls.Range.XMin + (resource_sfcls.Range.XMax - resource_sfcls.Range.XMin) / 2;
+                mode = SpaQueryMode.Contain;
+            }
+
+            //在ListView1控件第一列增加“OID”字段
+            FieldName("OID", listView);
+            FieldName("要素的长度", listView);
+
+            //将符合查询条件的要素存入RecordSet，然后进行遍历可以得到每一个要素的信息
+            _QueryDef = new QueryDef();
+            _QueryDef.SetRect(rect, mode);
+            _RecordSet = resource_sfcls.Select(_QueryDef);
+
+            bool rtn;
+            rtn = _RecordSet.MoveFirst();
+
+            while (!_RecordSet.IsEOF)
+            {
+                _Geometry = _RecordSet.Geometry;//获取当前要素的空间信息
+                oid = _RecordSet.CurrentID;
+                GeometryType type = _Geometry.Type;//获取当前要素的几何约束类型
+
+                if (_Geometry != null)
+                {
+                    switch (type)
+                    {
+                        case GeometryType.VarLine:
+                            {
+                                _GeoVarLine = new GeoVarLine();
+                                _GeoVarLine = _Geometry as GeoVarLine;
+                                length = _GeoVarLine.CalLength();
+                                break;
+                            }
+                        case GeometryType.Lines:
+                            {
+                                _GeoLines = new GeoLines();
+                                _GeoLines = _Geometry as GeoLines;
+                                length = _GeoLines.CalLength();
+                                break;
+                            }
+                    }
+                }
+                ListViewItem items = null;
+                items = listView.Items.Add(oid.ToString());
+                items.SubItems.Add(length.ToString());
+                rtn = _RecordSet.MoveNext();
+            }
+        }
+
+        public void FieldName(string name, ListView listView)
+        {
+            listView.Columns.Add(name, 120, HorizontalAlignment.Center);
         }
     }
 }
