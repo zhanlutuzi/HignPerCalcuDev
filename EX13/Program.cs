f3813fac4d71cdeabf838f0dc88864bf5cdedab2
zhanlutuzi <zhanlutuzi@gmail.com>
2022-10-19 13:28:11 +0800

    完成

--- EX13/Program.cs
+++ EX13/Program.cs
@@ -29,6 +29,7 @@ namespace EX13
             p.StartInfo.RedirectStandardError = true;
             //不显示程序窗口
             p.StartInfo.CreateNoWindow = true;
+
             //实例化p1进程
             Process p1 = new Process();
             //是否使用操作系统shell启动
@@ -44,8 +45,25 @@ namespace EX13
             p1.StartInfo.RedirectStandardError = true;
             //不显示程序窗口
             p1.StartInfo.CreateNoWindow = true;
+
+            //实例化p2进程
+            Process p2 = new Process();
+            //设置要启动的应用程序
+            p2.StartInfo.FileName = @"D:\Desktop\code\Learning\HignPerCalcuDev\HignPerCalcuDev\EX13\bin\external\netcoreapp3.1\EX13_Pro3.exe";
+            //是否使用操作系统shell启动
+            p2.StartInfo.UseShellExecute = false;
+            // 接收来自调用程序的输入信息
+            p2.StartInfo.RedirectStandardInput = true;
+            //输出信息
+            p2.StartInfo.RedirectStandardOutput = true;
+            // 输出错误
+            p2.StartInfo.RedirectStandardError = true;
+            //不显示程序窗口
+            p2.StartInfo.CreateNoWindow = true;
+
             p1.Start();
             p.Start();
+            p2.Start();
             p.WaitForExit();//等待程序执行完退出进程
             int code = p.ExitCode;
             if (code == 0)
@@ -54,6 +72,7 @@ namespace EX13
                 Console.WriteLine("进程1导出数据成功");
             }
             p.Close();
+
             p1.WaitForExit();
             int code1 = p1.ExitCode;
             if (code1 == 0)
@@ -62,6 +81,17 @@ namespace EX13
                 Console.WriteLine("进程2导出数据成功");
             }
             p1.Close();
+
+            p2.WaitForExit();
+            int code2 = p2.ExitCode;
+            if (code2 == 0)
+            {
+                Console.WriteLine("进程3导入数据成功");
+                Console.WriteLine("进程3导出数据成功");
+            }
+            p2.Close();
+
+
         }
 
     }
--- EX5/Form1.Designer.cs
+++ EX5/Form1.Designer.cs
@@ -42,10 +42,10 @@ namespace EX5
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 10);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(31, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 30);
+            this.button1.Size = new System.Drawing.Size(185, 38);
             this.button1.TabIndex = 9;
             this.button1.Text = "并行统计";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,20 +54,18 @@ namespace EX5
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(319, 48);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(425, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 24);
+            this.label2.Size = new System.Drawing.Size(112, 30);
             this.label2.TabIndex = 8;
             this.label2.Text = "线程二统计结果\r\n包含查询";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 48);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(27, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 24);
+            this.label1.Size = new System.Drawing.Size(112, 30);
             this.label1.TabIndex = 7;
             this.label1.Text = "线程一统计结果\r\n相交查询";
             // 
@@ -75,10 +73,10 @@ namespace EX5
             // 
             this.listView2.GridLines = true;
             this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(321, 80);
-            this.listView2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView2.Location = new System.Drawing.Point(428, 100);
+            this.listView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(279, 398);
+            this.listView2.Size = new System.Drawing.Size(371, 496);
             this.listView2.TabIndex = 6;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.View = System.Windows.Forms.View.Details;
@@ -87,10 +85,10 @@ namespace EX5
             // 
             this.listView1.GridLines = true;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(22, 80);
-            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Location = new System.Drawing.Point(12, 100);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(278, 398);
+            this.listView1.Size = new System.Drawing.Size(369, 496);
             this.listView1.TabIndex = 5;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -98,20 +96,18 @@ namespace EX5
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(918, 48);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(1224, 60);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 24);
+            this.label3.Size = new System.Drawing.Size(127, 30);
             this.label3.TabIndex = 13;
             this.label3.Text = "线程四统计结果\r\n最小外包矩形查询";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(619, 48);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(825, 60);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 24);
+            this.label4.Size = new System.Drawing.Size(112, 30);
             this.label4.TabIndex = 12;
             this.label4.Text = "线程三统计结果\r\n区域外查询";
             // 
@@ -119,10 +115,10 @@ namespace EX5
             // 
             this.listView3.GridLines = true;
             this.listView3.HideSelection = false;
-            this.listView3.Location = new System.Drawing.Point(621, 80);
-            this.listView3.Margin = new System.Windows.Forms.Padding(2);
+            this.listView3.Location = new System.Drawing.Point(828, 100);
+            this.listView3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listView3.Name = "listView3";
-            this.listView3.Size = new System.Drawing.Size(279, 398);
+            this.listView3.Size = new System.Drawing.Size(371, 496);
             this.listView3.TabIndex = 11;
             this.listView3.UseCompatibleStateImageBehavior = false;
             this.listView3.View = System.Windows.Forms.View.Details;
@@ -131,19 +127,19 @@ namespace EX5
             // 
             this.listView4.GridLines = true;
             this.listView4.HideSelection = false;
-            this.listView4.Location = new System.Drawing.Point(920, 80);
-            this.listView4.Margin = new System.Windows.Forms.Padding(2);
+            this.listView4.Location = new System.Drawing.Point(1227, 100);
+            this.listView4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listView4.Name = "listView4";
-            this.listView4.Size = new System.Drawing.Size(278, 398);
+            this.listView4.Size = new System.Drawing.Size(369, 496);
             this.listView4.TabIndex = 10;
             this.listView4.UseCompatibleStateImageBehavior = false;
             this.listView4.View = System.Windows.Forms.View.Details;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1211, 505);
+            this.ClientSize = new System.Drawing.Size(1615, 631);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.listView3);
@@ -153,7 +149,7 @@ namespace EX5
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.listView1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "EX5";
             this.ResumeLayout(false);
